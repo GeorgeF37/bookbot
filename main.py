9bfdb3e7b94b3631c4981e61d91d83b8fad1e6fa
GeorgeF37 <sinester66@yahoo.com>
2024-11-18 13:29:46 +0000

    O: Updated letter counter

--- main.py
+++ main.py
@@ -2,9 +2,22 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     the_count = word_counter(text)
-    print(f"\n{the_count} words in frankenstein.txt\n")
+    print('--- Begin report of books/frankenstein.txt ---')
+    print(f"{the_count} words found in the document\n")
     letter = letter_counter(text)
-    print(f"{letter} \n")
+    
+    sorted_letter = []
+    for l in letter:
+        if l.isalpha():
+            sorted_letter.append({'name':l,'num':letter[l]})
+    
+    sorted_letter.sort(reverse=True, key=sorter)
+    
+    for i in sorted_letter:
+        print(f"The '{i['name']}' character was found {i['num']} times.")
+
+    print('--- End report ---')
+
 
 def get_book_text(path):
     with open(path) as f:
@@ -12,10 +25,6 @@ def get_book_text(path):
 
 def word_counter(text):
     words = text.split()
-    '''counter = 0
-    for word in words:
-        counter += 1
-    print(counter)'''
     return len(words)
 
 def letter_counter(text):
@@ -23,9 +32,12 @@ def letter_counter(text):
     the_letters = {}
     for letter in the_book:
         if letter not in the_letters:
-            the_letters.update({letter: + 1})
+            the_letters.update({letter: 1})
         elif letter in the_letters:
             the_letters[letter] += 1
     return the_letters
+
+def sorter(dict):
+    return dict["num"]
 
 main()
